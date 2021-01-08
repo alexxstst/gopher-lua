13445720af6b990a7204402bcc5accf5941052e3
alexxst.st <alexxst.st@yandex.ru>
2021-01-08 13:19:25 +0300

    add mapper for lua structures

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/chzyer/logex v1.1.10 // indirect
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
 	github.com/chzyer/test v0.0.0-20180213035817-a1ea475d72b1 // indirect
+	github.com/mitchellh/mapstructure v1.4.0
 	golang.org/x/sys v0.0.0-20190204203706-41f3e6584952 // indirect
 )
--- performance_test.go
+++ performance_test.go
@@ -18,3 +18,16 @@ func TestPerformanceLua(t *testing.T) {
 	duration := time.Since(start)
 	fmt.Println(duration.Milliseconds())
 }
+
+func TestPerformanceLua_2(t *testing.T) {
+	var luaCode = "local function fib(n)\n if n < 2 then return n end\n    return fib(n - 2) + fib(n - 1)\nend\n\n for i = 1, 100 do fib(10) \nend"
+	fmt.Println("Start golua-fib")
+	start := time.Now()
+	L := NewState()
+	defer L.Close()
+	if err := L.DoString(luaCode); err != nil {
+		panic(err)
+	}
+	duration := time.Since(start)
+	fmt.Println(duration.Milliseconds())
+}
